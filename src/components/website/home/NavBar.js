63e39efcee800937a3463a1675f258666150445b
Imtiaj Shuvo <imtiajrex@gmail.com>
2021-04-16 16:33:58 +0600

    removed navbar container

--- src/components/website/home/NavBar.js
+++ src/components/website/home/NavBar.js
@@ -1,101 +1,97 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
 import {
-  Collapse,
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-  Nav,
-  Navbar,
-  NavbarToggler,
-  NavItem,
+	Collapse,
+	Dropdown,
+	DropdownItem,
+	DropdownMenu,
+	DropdownToggle,
+	Nav,
+	Navbar,
+	NavbarToggler,
+	NavItem,
 } from "reactstrap";
 import { NavLink as RNavLink } from "react-router-dom";
 import MenuBar from "./MenuBar";
 
 export default function NavBar({ pages, sub_pages, institute_info }) {
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => setIsOpen(!isOpen);
+	const toggle = () => setIsOpen(!isOpen);
 
-  const [dropdownOpen, setDropdown] = useState();
+	const [dropdownOpen, setDropdown] = useState();
 
-  const toggleDropdown = () => setDropdown(!dropdownOpen);
-  return (
-    <>
-      <Navbar light expand="md" className="probootstrap-navbar">
-        <div className="container" style={{ alignItems: "normal" }}>
-          <div className="row" style={{ width: "100%" }}>
-            <div className="col-md-11 col-sm-12">
-              <div
-                style={{
-                  height: "auto",
-                  display: "flex",
-                  alignItems: "center",
-                }}
-              >
-                <RNavLink
-                  to="/"
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  <img
-                    src={
-                      process.env.REACT_APP_IMAGE_PATH +
-                      "/" +
-                      institute_info.institute_logo
-                    }
-                    alt="Logo"
-                    style={{ width: "80px", objectFit: "cover" }}
-                  />
-                  <div className="ml-3 institute_name">
-                    {institute_info.institute_name}
-                  </div>
-                </RNavLink>
-              </div>
-            </div>
-            <div className="col-md-1 col-sm-12 nav-details">
-              <span>
-                <i className="icon-location2"></i>
-                {institute_info.institute_address}
-              </span>
-              <span>
-                <i className="icon-phone2"></i>
-                {institute_info.institute_phonenumbers}
-              </span>
-              <span>
-                <i className="icon-mail"></i>
-                {institute_info.institute_email}
-              </span>
-            </div>
-          </div>
-        </div>
-      </Navbar>
-      <Navbar
-        light
-        expand="md"
-        className="probootstrap-navbar"
-        style={{ backgroundColor: "#49d292" }}
-      >
-        <div className="container" style={{ alignItems: "normal" }}>
-          <MenuBar
-            pages={pages}
-            sub_pages={sub_pages}
-            isOpen={isOpen}
-            setIsOpen={setIsOpen}
-            dropdownOpen={dropdownOpen}
-            toggleDropdown={toggleDropdown}
-            setDropdown={setDropdown}
-          />
-          <div className="col-md-12">
-            <NavbarToggler onClick={toggle} />
-          </div>
-        </div>
-      </Navbar>
-    </>
-  );
+	const toggleDropdown = () => setDropdown(!dropdownOpen);
+	return (
+		<>
+			<Navbar light expand="md" className="probootstrap-navbar">
+				<div className="row" style={{ width: "100%" }}>
+					<div className="col-md-11 col-sm-12">
+						<div
+							style={{
+								height: "auto",
+								display: "flex",
+								alignItems: "center",
+							}}
+						>
+							<RNavLink
+								to="/"
+								style={{
+									display: "flex",
+									alignItems: "center",
+									justifyContent: "center",
+								}}
+							>
+								<img
+									src={
+										process.env.REACT_APP_IMAGE_PATH +
+										"/" +
+										institute_info.institute_logo
+									}
+									alt="Logo"
+									style={{ width: "80px", objectFit: "cover" }}
+								/>
+								<div className="ml-3 institute_name">
+									{institute_info.institute_name}
+								</div>
+							</RNavLink>
+						</div>
+					</div>
+					<div className="col-md-1 col-sm-12 nav-details">
+						<span>
+							<i className="icon-location2"></i>
+							{institute_info.institute_address}
+						</span>
+						<span>
+							<i className="icon-phone2"></i>
+							{institute_info.institute_phonenumbers}
+						</span>
+						<span>
+							<i className="icon-mail"></i>
+							{institute_info.institute_email}
+						</span>
+					</div>
+				</div>
+			</Navbar>
+			<Navbar
+				light
+				expand="md"
+				className="probootstrap-navbar"
+				style={{ backgroundColor: "#49d292" }}
+			>
+				<MenuBar
+					pages={pages}
+					sub_pages={sub_pages}
+					isOpen={isOpen}
+					setIsOpen={setIsOpen}
+					dropdownOpen={dropdownOpen}
+					toggleDropdown={toggleDropdown}
+					setDropdown={setDropdown}
+				/>
+				<div className="col-md-12">
+					<NavbarToggler onClick={toggle} />
+				</div>
+			</Navbar>
+		</>
+	);
 }
