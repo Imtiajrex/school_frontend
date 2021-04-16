d82df9897f62944eb5e195af057d94063ceb322b
Imtiaj Shuvo <imtiajrex@gmail.com>
2021-04-16 16:43:07 +0600

    Updated Navbar

--- src/components/website/home/NavBar.js
+++ src/components/website/home/NavBar.js
@@ -67,7 +67,12 @@ export default function NavBar({ pages, sub_pages, institute_info }) {
 				light
 				expand="md"
 				className="probootstrap-navbar"
-				style={{ backgroundColor: "#49d292" }}
+				style={{
+					backgroundColor: "#49d292",
+					justifyContent: "center",
+					paddingTop: 0,
+					paddingBottom: 0,
+				}}
 			>
 				<MenuBar
 					pages={pages}
