0faa23465ee1801eeeb076a5b7e6daec003308da
Imtiaj Shuvo <imtiajrex@gmail.com>
2021-05-04 21:19:54 +0600

    Updated Admit Card Design With Graphics

--- src/components/crud/Index.js
+++ src/components/crud/Index.js
@@ -13,283 +13,283 @@ import List from "./List";
 import Query from "./Query";
 
 function Index(props) {
-  const {
-    title,
-    add_data = [],
-    add_initial_values = [],
-    CustomListComponent = undefined,
-    custom_list = undefined,
-    custom_update = undefined,
-    setCustomUpdate = undefined,
-    query_list = undefined,
-    custom_loading = undefined,
-    modal_size = "sm",
-    list_url,
-    list_head,
-    list_active = true,
-    add = false,
-    edit = false,
-    remove = false,
-    edit_data = [],
-    query_title,
-    query_data,
-    indexed = true,
-    file = false,
-    print_url = "",
-    add_button_title = "Add New Record",
-    def_url_param = "",
-    ...other
-  } = props;
-  const [openAdd, setOpenAdd] = useState(false);
-  const [openEdit, setOpenEdit] = useState(false);
-  const [openDelete, setOpenDelete] = useState(false);
-  const [list, setList] = useState([]);
-  const [deleteInfo, setDeleteInfo] = useState("");
-  const [editInfo, setEditInfo] = useState({});
-  const [update, setUpdate] = useState(true);
-  const [loading, setloading] = useState(false);
-  const [query, setquery] = useState("");
-  const [query_tags, setQueryTags] = useState([]);
-  const [querying, setQuerying] = useState(false);
+	const {
+		title,
+		add_data = [],
+		add_initial_values = [],
+		CustomListComponent = undefined,
+		custom_list = undefined,
+		custom_update = undefined,
+		setCustomUpdate = undefined,
+		query_list = undefined,
+		custom_loading = undefined,
+		modal_size = "sm",
+		list_url,
+		list_head,
+		list_active = true,
+		add = false,
+		edit = false,
+		remove = false,
+		edit_data = [],
+		query_title,
+		query_data,
+		indexed = true,
+		file = false,
+		print_url = "",
+		add_button_title = "Add New Record",
+		def_url_param = "",
+		...other
+	} = props;
+	const [openAdd, setOpenAdd] = useState(false);
+	const [openEdit, setOpenEdit] = useState(false);
+	const [openDelete, setOpenDelete] = useState(false);
+	const [list, setList] = useState([]);
+	const [deleteInfo, setDeleteInfo] = useState("");
+	const [editInfo, setEditInfo] = useState({});
+	const [update, setUpdate] = useState(true);
+	const [loading, setloading] = useState(false);
+	const [query, setquery] = useState("");
+	const [query_tags, setQueryTags] = useState([]);
+	const [querying, setQuerying] = useState(false);
 
-  React.useEffect(() => {
-    if (custom_list === undefined && list_active) {
-      if (typeof query_list == "object") {
-        if (query.length > 0) {
-          setloading(true);
-          setQuerying(true);
-          Api({
-            method: "get",
-            url: list_url + query + def_url_param,
-          })
-            .then((res) => {
-              setList(res.data);
-              setQuerying(false);
-              setloading(false);
-            })
-            .catch((error) => {
-              setQuerying(false);
-              setloading(false);
-            });
-        }
-      } else {
-        setloading(true);
-        Api({
-          method: "get",
-          url: list_url + "?" + def_url_param,
-        })
-          .then((res) => {
-            setList(res.data);
-            setloading(false);
-          })
-          .catch((error) => {
-            setQuerying(false);
-            setloading(false);
-          });
-      }
-    } else {
-      if (setCustomUpdate !== undefined) setCustomUpdate(!custom_update);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [update, list_url]);
-  React.useEffect(() => {
-    if (Object.keys(editInfo).length > 0) setOpenEdit(true);
-    else setOpenEdit(false);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [editInfo]);
-  return (
-    <>
-      <Container fluid>
-        <Helmet>
-          <title>{title}</title>
-        </Helmet>
-        {query_list != null && query_list.length > 0 ? (
-          <Row className="mt-5 mb-5">
-            <div className="col">
-              <Card
-                className="bg-default shadow"
-                style={{ maxWidth: "450px", margin: "auto" }}
-              >
-                <CardHeader className="bg-transparent border-0">
-                  <h3 className="text-white mb-0">{query_title}</h3>
-                </CardHeader>
-                <CardBody>
-                  <Query
-                    query_list={query_list}
-                    initial_values={query_data}
-                    query={query}
-                    setquery={setquery}
-                    update={update}
-                    setUpdate={setUpdate}
-                    setQueryTags={setQueryTags}
-                    querying={querying}
-                  />
-                </CardBody>
-              </Card>
-            </div>
-          </Row>
-        ) : null}
+	React.useEffect(() => {
+		if (custom_list === undefined && list_active) {
+			if (typeof query_list == "object") {
+				if (query.length > 0) {
+					setloading(true);
+					setQuerying(true);
+					Api({
+						method: "get",
+						url: list_url + query + def_url_param,
+					})
+						.then((res) => {
+							setList(res.data);
+							setQuerying(false);
+							setloading(false);
+						})
+						.catch((error) => {
+							setQuerying(false);
+							setloading(false);
+						});
+				}
+			} else {
+				setloading(true);
+				Api({
+					method: "get",
+					url: list_url + "?" + def_url_param,
+				})
+					.then((res) => {
+						setList(res.data);
+						setloading(false);
+					})
+					.catch((error) => {
+						setQuerying(false);
+						setloading(false);
+					});
+			}
+		} else {
+			if (setCustomUpdate !== undefined) setCustomUpdate(!custom_update);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [update, list_url]);
+	React.useEffect(() => {
+		if (Object.keys(editInfo).length > 0) setOpenEdit(true);
+		else setOpenEdit(false);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [editInfo]);
+	return (
+		<>
+			<Container fluid>
+				<Helmet>
+					<title>{title}</title>
+				</Helmet>
+				{query_list != null && query_list.length > 0 ? (
+					<Row className="mt-5 mb-5">
+						<div className="col">
+							<Card
+								className="bg-default shadow"
+								style={{ maxWidth: "450px", margin: "auto" }}
+							>
+								<CardHeader className="bg-transparent border-0">
+									<h3 className="text-white mb-0">{query_title}</h3>
+								</CardHeader>
+								<CardBody>
+									<Query
+										query_list={query_list}
+										initial_values={query_data}
+										query={query}
+										setquery={setquery}
+										update={update}
+										setUpdate={setUpdate}
+										setQueryTags={setQueryTags}
+										querying={querying}
+									/>
+								</CardBody>
+							</Card>
+						</div>
+					</Row>
+				) : null}
 
-        <Row className="mt-5 mb-5">
-          <div className="col">
-            <Card className="bg-default shadow">
-              <CardHeader className="bg-transparent border-0">
-                <h3 className="text-white mb-0">{title}</h3>
-                {print_url.length > 0 ? (
-                  query.length > 0 ? (
-                    <Button
-                      color="white"
-                      className="m-2"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        window.open(
-                          "/print/" + print_url + query,
-                          "Print Receipt",
-                          "height=600,width=800"
-                        );
-                        return false;
-                      }}
-                    >
-                      Print
-                    </Button>
-                  ) : null
-                ) : null}
+				<Row className="mt-5 mb-5">
+					<div className="col">
+						<Card className="bg-default shadow">
+							<CardHeader className="bg-transparent border-0">
+								<h3 className="text-white mb-0">{title}</h3>
+								{print_url.length > 0 ? (
+									query.length > 0 ? (
+										<Button
+											color="white"
+											className="m-2"
+											onClick={(e) => {
+												e.preventDefault();
+												window.open(
+													"/print/" + print_url + query,
+													"Print Receipt",
+													"height=600,width=1280"
+												);
+												return false;
+											}}
+										>
+											Print
+										</Button>
+									) : null
+								) : null}
 
-                {add ? (
-                  <Button
-                    color="primary"
-                    style={{ display: "flex", alignItems: "center" }}
-                    onClick={() => setOpenAdd(!openAdd)}
-                  >
-                    {add_button_title}
-                    <i
-                      className="ni ni-fat-add"
-                      style={{ fontSize: "1.5rem" }}
-                    />
-                  </Button>
-                ) : null}
-                <div
-                  style={{
-                    width: "100%",
-                    display: "flex",
-                    flexWrap: "wrap",
-                    marginTop: "1rem",
-                  }}
-                >
-                  {query_tags.length > 0
-                    ? query_tags.map((element, index) => (
-                        <Button
-                          key={index}
-                          color="success"
-                          size="sm"
-                          style={{
-                            display: "flex",
-                            alignItems: "center",
-                            maxWidth: "250px",
-                            marginRight: "1rem",
-                          }}
-                        >
-                          {element.title} : {element.value}
-                        </Button>
-                      ))
-                    : null}
-                </div>
-                <Button
-                  color="white"
-                  size="sm"
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    maxWidth: "250px",
-                    marginRight: "1rem",
-                    marginTop: "1rem",
-                  }}
-                >
-                  Total Records : {list.length}
-                </Button>
-              </CardHeader>
-              {list_active ? (
-                CustomListComponent == undefined ? (
-                  <List
-                    setOpenDelete={setOpenDelete}
-                    setOpenEdit={setOpenEdit}
-                    setDeleteInfo={setDeleteInfo}
-                    setEditInfo={setEditInfo}
-                    list={custom_list === undefined ? list : custom_list}
-                    list_head={list_head}
-                    edit={edit}
-                    remove={remove}
-                    loading={
-                      custom_loading !== undefined ? custom_loading : loading
-                    }
-                    indexed={indexed}
-                    {...other}
-                  />
-                ) : (
-                  <CustomListComponent
-                    list={list}
-                    list_head={list_head}
-                    query_tags={query_tags}
-                    query={query}
-                    loading={loading}
-                    url={list_url}
-                    setOpenDelete={setOpenDelete}
-                    setOpenEdit={setOpenEdit}
-                    setDeleteInfo={setDeleteInfo}
-                    setEditInfo={setEditInfo}
-                    update={update}
-                    setupdate={setUpdate}
-                    edit={edit}
-                    remove={remove}
-                    indexed={indexed}
-                    {...other}
-                  />
-                )
-              ) : null}
-            </Card>
-          </div>
-        </Row>
-        {edit && Object.keys(editInfo).length > 0 ? (
-          <Edit
-            open={openEdit}
-            setOpenEdit={setOpenEdit}
-            edit_values={editInfo}
-            update={update}
-            setUpdate={setUpdate}
-            edit_data={edit_data}
-            url={list_url}
-            file={file}
-            setEditInfo={setEditInfo}
-            modal_size={modal_size}
-          />
-        ) : null}
-        {add ? (
-          <Add
-            url={list_url}
-            open={openAdd}
-            setOpenAdd={setOpenAdd}
-            add_data={add_data}
-            initial_values={add_initial_values}
-            update={update}
-            setUpdate={setUpdate}
-            modal_size={modal_size}
-            file={file}
-          />
-        ) : null}
+								{add ? (
+									<Button
+										color="primary"
+										style={{ display: "flex", alignItems: "center" }}
+										onClick={() => setOpenAdd(!openAdd)}
+									>
+										{add_button_title}
+										<i
+											className="ni ni-fat-add"
+											style={{ fontSize: "1.5rem" }}
+										/>
+									</Button>
+								) : null}
+								<div
+									style={{
+										width: "100%",
+										display: "flex",
+										flexWrap: "wrap",
+										marginTop: "1rem",
+									}}
+								>
+									{query_tags.length > 0
+										? query_tags.map((element, index) => (
+												<Button
+													key={index}
+													color="success"
+													size="sm"
+													style={{
+														display: "flex",
+														alignItems: "center",
+														maxWidth: "250px",
+														marginRight: "1rem",
+													}}
+												>
+													{element.title} : {element.value}
+												</Button>
+										  ))
+										: null}
+								</div>
+								<Button
+									color="white"
+									size="sm"
+									style={{
+										display: "flex",
+										alignItems: "center",
+										maxWidth: "250px",
+										marginRight: "1rem",
+										marginTop: "1rem",
+									}}
+								>
+									Total Records : {list.length}
+								</Button>
+							</CardHeader>
+							{list_active ? (
+								CustomListComponent == undefined ? (
+									<List
+										setOpenDelete={setOpenDelete}
+										setOpenEdit={setOpenEdit}
+										setDeleteInfo={setDeleteInfo}
+										setEditInfo={setEditInfo}
+										list={custom_list === undefined ? list : custom_list}
+										list_head={list_head}
+										edit={edit}
+										remove={remove}
+										loading={
+											custom_loading !== undefined ? custom_loading : loading
+										}
+										indexed={indexed}
+										{...other}
+									/>
+								) : (
+									<CustomListComponent
+										list={list}
+										list_head={list_head}
+										query_tags={query_tags}
+										query={query}
+										loading={loading}
+										url={list_url}
+										setOpenDelete={setOpenDelete}
+										setOpenEdit={setOpenEdit}
+										setDeleteInfo={setDeleteInfo}
+										setEditInfo={setEditInfo}
+										update={update}
+										setupdate={setUpdate}
+										edit={edit}
+										remove={remove}
+										indexed={indexed}
+										{...other}
+									/>
+								)
+							) : null}
+						</Card>
+					</div>
+				</Row>
+				{edit && Object.keys(editInfo).length > 0 ? (
+					<Edit
+						open={openEdit}
+						setOpenEdit={setOpenEdit}
+						edit_values={editInfo}
+						update={update}
+						setUpdate={setUpdate}
+						edit_data={edit_data}
+						url={list_url}
+						file={file}
+						setEditInfo={setEditInfo}
+						modal_size={modal_size}
+					/>
+				) : null}
+				{add ? (
+					<Add
+						url={list_url}
+						open={openAdd}
+						setOpenAdd={setOpenAdd}
+						add_data={add_data}
+						initial_values={add_initial_values}
+						update={update}
+						setUpdate={setUpdate}
+						modal_size={modal_size}
+						file={file}
+					/>
+				) : null}
 
-        {remove ? (
-          <Delete
-            open={openDelete}
-            setOpenDelete={setOpenDelete}
-            id={deleteInfo}
-            update={update}
-            setUpdate={setUpdate}
-            url={list_url}
-          />
-        ) : null}
-      </Container>
-    </>
-  );
+				{remove ? (
+					<Delete
+						open={openDelete}
+						setOpenDelete={setOpenDelete}
+						id={deleteInfo}
+						update={update}
+						setUpdate={setUpdate}
+						url={list_url}
+					/>
+				) : null}
+			</Container>
+		</>
+	);
 }
 
 export default Index;
--- src/components/print/docs/admit/AdCard.js
+++ src/components/print/docs/admit/AdCard.js
@@ -1,95 +1,237 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import { Table } from "reactstrap";
-
+import CardBackground from "./bg.png";
 export default function AdCard(props) {
-  const { data, colors, size, exam_data } = props;
-  const { logo_size, head_size, data_size } = size;
-  const { head_color, data_color, border_color } = colors;
-  const th_style = {
-    padding: "0.4rem",
-    color: head_color,
-    fontSize: head_size + "px",
-    borderTop: "1px solid " + border_color,
-  };
-  const td_style = {
-    padding: "0.4rem",
-    color: data_color,
-    fontSize: data_size + "px",
-    borderTop: "1px solid " + border_color,
-  };
-  return (
-    <div style={{ position: "relative" }}>
-      <div
-        style={{
-          position: "absolute",
-          top: "0%",
-          right: "0%",
-          margin: "1rem",
-        }}
-      >
-        <img
-          src={process.env.REACT_APP_IMAGE_PATH + "/" + data.student_image}
-          alt="Student"
-          style={{ width: logo_size + "px", objectFit: "cover" }}
-        />
-      </div>
-      <div style={{ textAlign: "center" }}>
-        <b
-          style={{
-            fontSize: head_size + "px",
-            textAlign: "center",
-            color: head_color,
-            margin: "0.3rem",
-          }}
-        >
-          Admit Card
-        </b>
-      </div>
-      <Table style={{ maxWidth: "65%", margin: "auto" }}>
-        <tbody>
-          <tr>
-            <th style={th_style}>Student ID</th>
-            <td style={td_style}>{data.student_identifier}</td>
-            <th style={th_style}>Student Name</th>
-            <td style={td_style}>{data.student_name}</td>
-          </tr>
-          <tr>
-            <th style={th_style}>Roll</th>
-            <td style={td_style}>{data.role}</td>
-            <th style={th_style}>Class</th>
-            <td style={td_style}>{data.class}</td>
-          </tr>
-          <tr>
-            <th style={th_style}>Department</th>
-            <td style={td_style}>{data.department}</td>
-            <th style={th_style}>Session</th>
-            <td style={td_style}>{data.session}</td>
-          </tr>
-          <tr className="text-center">
-            <th colSpan="2" style={th_style}>
-              Exam
-            </th>
-            <td colSpan="2" style={td_style}>
-              {exam_data.exam_name}
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-      <div
-        className="d-flex"
-        style={{
-          maxWidth: "65%",
-          justifyContent: "space-between",
-          margin: "2.5rem auto",
-        }}
-      >
-        <div style={{ ...th_style, borderTop: "1px dotted black" }}>
-          Exam Controller
-        </div>
-        <div style={{ ...th_style, borderTop: "1px dotted black" }}>
-          Office Executive
-        </div>
-      </div>
-    </div>
-  );
+	const { data, colors, size, exam_data, inf } = props;
+	const { logo_size, head_size, data_size } = size;
+	const { head_color, data_color, border_color } = colors;
+	const th_style = {
+		padding: "0.4rem",
+		color: head_color,
+		fontSize: head_size + "px",
+		borderTop: "1px solid " + border_color,
+	};
+	const td_style = {
+		padding: "0.4rem",
+		color: data_color,
+		fontSize: data_size + "px",
+		borderTop: "1px solid " + border_color,
+	};
+	return (
+		<div
+			style={{
+				padding: "2.5rem",
+				height: "650px",
+				position: "relative",
+				margin: "auto",
+				width: "100%",
+			}}
+		>
+			<img
+				src={CardBackground}
+				style={{
+					width: "100%",
+					position: "absolute",
+					top: 0,
+					left: 0,
+					zIndex: -999,
+				}}
+			/>
+			<div style={{ width: "100%", position: "relative", height: "120px" }}>
+				<img
+					src={process.env.REACT_APP_IMAGE_PATH + "/" + data.student_image}
+					alt="Student"
+					style={{
+						width: logo_size + "px",
+						objectFit: "cover",
+						position: "absolute",
+						top: 0,
+						right: 0,
+					}}
+				/>
+				<img
+					src={process.env.REACT_APP_IMAGE_PATH + "/" + inf.institute_logo}
+					alt="Institute Logo"
+					style={{
+						width: logo_size + "px",
+						objectFit: "cover",
+						position: "absolute",
+						top: 0,
+						left: 0,
+					}}
+				/>
+				<div
+					style={{
+						position: "absolute",
+						top: 0,
+						left: "50%",
+						transform: "translateX(-50%)",
+						textAlign: "center",
+						width: "100%",
+					}}
+				>
+					<h1
+						style={{
+							fontSize: "28px",
+							textTransform: "uppercase",
+							fontWeight: "bold",
+						}}
+					>
+						{inf.institute_name}
+					</h1>
+					<h2 style={{ fontSize: "13px" }}>{inf.institute_address}</h2>
+					<h2 style={{ fontSize: "13px" }}>
+						Phonenumber: {inf.institute_phonenumbers} | Email:{" "}
+						{inf.institute_email}
+					</h2>
+				</div>
+			</div>
+
+			<div style={{ width: "100%" }}>
+				<div
+					style={{
+						padding: "0.5rem 1rem",
+						width: "120px",
+						margin: "auto",
+						backgroundColor: "#2ca887",
+						color: "white",
+					}}
+				>
+					Admit Card
+				</div>
+				<div
+					style={{
+						textAlign: "center",
+					}}
+				>
+					{exam_data.exam_name}
+				</div>
+				<div
+					style={{
+						maxWidth: "70%",
+						display: "flex",
+						justifyContent: "center",
+						marginTop: "2rem",
+						flexDirection: "column",
+
+						margin: "auto",
+					}}
+				>
+					<div
+						style={{
+							display: "flex",
+							justifyContent: "space-between",
+							marginBottom: "1.5rem",
+						}}
+					>
+						<div>
+							<b>Centre Name:</b> {inf.institute_name}
+						</div>
+						<div>
+							<b>Class:</b>{" "}
+							<a
+								style={{
+									padding: "0.5rem",
+									marginLeft: "0.2rem",
+									border: "1px solid grey",
+								}}
+							>
+								{data.class}
+							</a>
+						</div>
+					</div>
+					<div
+						style={{
+							display: "flex",
+							justifyContent: "space-between",
+							marginBottom: "1.5rem",
+							width: "100%",
+						}}
+					>
+						<div style={{ display: "flex", justifyContent: "space-between" }}>
+							<b>Name Of Examinee:</b>
+							<a style={{ marginLeft: "1rem" }}>{data.student_name}</a>
+						</div>
+						<div>
+							<b>ID No:</b>
+							<a
+								style={{
+									padding: "0.5rem",
+									marginLeft: "0.2rem",
+									border: "1px solid grey",
+								}}
+							>
+								{data.student_identifier}
+							</a>
+						</div>
+					</div>
+					<div
+						style={{
+							display: "flex",
+							justifyContent: "space-between",
+							marginBottom: "1.5rem",
+						}}
+					>
+						<div style={{ width: "100%" }}>
+							<b>Father's Name:</b>
+
+							<a style={{ marginLeft: "1rem" }}>{data.father_name}</a>
+						</div>
+						<div>
+							<b>Session:</b>
+							<a
+								style={{
+									padding: "0.3rem",
+									marginLeft: "0.2rem",
+									border: "1px solid grey",
+								}}
+							>
+								{data.session}
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div
+				style={{
+					position: "absolute",
+					bottom: "15%",
+					display: "flex",
+					justifyContent: "space-between",
+					fontWeight: "bold",
+					width: "90%",
+				}}
+			>
+				<div>
+					Type of the examinee: <a style={{ color: "#fc0352" }}>REGULAR</a>
+				</div>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						textAlign: "center",
+					}}
+				>
+					<div
+						style={{
+							fontSize: "19px",
+							padding: "0.5rem 0.2rem",
+							borderTop: "1px solid grey",
+						}}
+					>
+						Head Teacher
+					</div>
+					<div
+						style={{
+							fontSize: "13px",
+						}}
+					>
+						{inf.institute_name}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
